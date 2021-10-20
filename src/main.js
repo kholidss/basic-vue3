d33265103f8aeceb4cb681a83f591e95ee71a067
kholid saifulloh <muhammadkholid@refactory.id>
2021-10-20 20:36:37 +0700

    add v if

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import DeclarativeRendering from './1-DeclarativeRendering.vue'
 import Methods from './2-Methods.vue'
 import VModel from './3-VModel.vue'
+import VIf from './4-VIf.vue'
 
 // createApp(DeclarativeRendering).mount('#app')
 // createApp(Methods).mount('#app')
-createApp(VModel).mount('#app')
+// createApp(VModel).mount('#app')
+createApp(VIf).mount('#app')
