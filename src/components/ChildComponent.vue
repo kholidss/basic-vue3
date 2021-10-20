578aa4d1695e5b4c55005bbec0361d452e563c6b
kholid saifulloh <muhammadkholid@refactory.id>
2021-10-20 21:37:00 +0700

    add props in component

--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -1,3 +1,15 @@
 <template>
   <div>This is child component</div>
+  <div>{{ text }}</div>
 </template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
