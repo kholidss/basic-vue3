4c58549db35ee2687c96f83aa842168658f10ed0
kholid saifulloh <muhammadkholid@refactory.id>
2021-10-20 15:00:33 +0700

    add methods

---
+++ src/2-Methods.vue
@@ -0,0 +1,30 @@
+
+
+<template>
+<!-- declarative rendering -->
+  <div>{{ count }}</div>
+
+  <button @click='count++'>Add 1</button>
+
+  <!-- method digunakan untuk menghandle sebuah aksi dari inputan user -->
+  <button @click='addCount'>Add 2</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+
+  methods: {
+    addCount() {
+      this.count += 2;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import DeclarativeRendering from './1-DeclarativeRendering.vue'
+import Methods from './2-Methods.vue'
 
-createApp(DeclarativeRendering).mount('#app')
+// createApp(DeclarativeRendering).mount('#app')
+createApp(Methods).mount('#app')
