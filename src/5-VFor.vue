62a2cfbf8905becbe5f7d83049c2f65e1ba520c9
kholid saifulloh <muhammadkholid@refactory.id>
2021-10-20 20:45:01 +0700

    add v for

---
+++ src/5-VFor.vue
@@ -0,0 +1,29 @@
+<template>
+  <ol>
+    <li v-for="(todo, index) in todos" :key="todo.id">
+      {{ todo.activity }} - {{ index }}
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [
+        { id: 1, activity: "Masak" },
+        { id: 2, activity: "Memanggang" },
+        { id: 3, activity: "Mencuci" },
+      ],
+    };
+  },
+
+  methods: {
+    setStatus() {
+      this.status = !this.status;
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import DeclarativeRendering from './1-DeclarativeRendering.vue'
 import Methods from './2-Methods.vue'
 import VModel from './3-VModel.vue'
 import VIf from './4-VIf.vue'
+import VFor from './5-VFor.vue'
 
 // createApp(DeclarativeRendering).mount('#app')
 // createApp(Methods).mount('#app')
 // createApp(VModel).mount('#app')
-createApp(VIf).mount('#app')
+// createApp(VIf).mount('#app')
+createApp(VFor).mount('#app')
