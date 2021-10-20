8e98e5ebb1f0aca134aefb4fe9fdd6728995d765
kholid saifulloh <muhammadkholid@refactory.id>
2021-10-20 18:13:20 +0700

    add v model

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import DeclarativeRendering from './1-DeclarativeRendering.vue'
 import Methods from './2-Methods.vue'
+import VModel from './3-VModel.vue'
 
 // createApp(DeclarativeRendering).mount('#app')
-createApp(Methods).mount('#app')
+// createApp(Methods).mount('#app')
+createApp(VModel).mount('#app')
